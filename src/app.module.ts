fa91ec62803642b49b35e9cb4fc0b400cdb208db
mostafaroshdy1 <mostafaroshdy1@gmail.com>
2024-10-22 21:58:39 +0300

    feat: add global 50 req/minute rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from './common/config/configuration';
 import { UserModule } from './modules/user.module';
 import { AuthModule } from './modules/auth.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { CurrentUserInterceptor } from './common/interceptors/currentUser.interceptor';
 import { CommonModule } from './modules/common.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -18,12 +19,23 @@ import { CommonModule } from './modules/common.module';
     UserModule,
     AuthModule,
     CommonModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 50,
+      },
+    ]),
   ],
 
   providers: [
     {
       provide: APP_INTERCEPTOR,
       useClass: CurrentUserInterceptor,
+    },
+
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
   ],
 })
